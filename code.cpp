554c3595026719feedf2a80c283987e00f90d6e9
Миненко Алексей Сергеевич ТСА-143 <minenko_alex@bk.ru>
2025-05-28 10:28:11 +0300

    Add files via upload

---
+++ code.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <limits> 
+using namespace std;
+
+/**
+ * @brief Перевод веса из граммов в унции
+ * @param gramms вес в граммах
+ * @param ounces константа для перевода (1 унция = 28.3 грамма)
+ * @return возвращает значение в унциях 
+ */
+double gramms_to_ounces(double gramms, double const ounces);
+
+/**
+ * @brief Запрашивает ввод данных с клавиатуры
+ * @return возвращает значение в граммах
+ */
+double get_input();
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    const double ounces = 28.3;
+    
+    double gramms = get_input();
+    if (gramms < 0) {
+        cout << "Ошибка: вес не может быть отрицательным!" << endl;
+        return 1;
+    }
+    
+    cout << "Ввод выполнен корректно!" << endl;
+    cout << "Вес в унциях: " << gramms_to_ounces(gramms, ounces) << endl;
+    
+    return 0;
+}
+
+double gramms_to_ounces(double gramms, double const ounces) {
+    return gramms / ounces;
+}
+
+double get_input() {
+    double gramms = 0;
+    cout << "Введите вес в граммах: ";
+    cin >> gramms;
+    
+    // Проверка на корректность ввода
+    while (cin.fail()) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Ошибка ввода. Пожалуйста, введите число: ";
+        cin >> gramms;
+    }
+    
+    return gramms;
+}
