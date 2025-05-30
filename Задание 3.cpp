f99567e53b8a3a8ba867a870a56125dfe05ef80b
Миненко Алексей Сергеевич ТСА-143 <minenko_alex@bk.ru>
2025-05-30 16:27:01 +0300

    Update Задание 3.cpp

--- Задание 3.cpp
+++ Задание 3.cpp
@@ -1,94 +1,110 @@
-﻿#include <iostream>
+#include <iostream>
+#include <limits>
 #include <stdexcept>
 
 using namespace std;
 
 /**
-*@brief Считывает значиния с клавиатуры с проверкой ввода
-*@return возвращает значение, если оно правильное , иначе завершает программу
+* @brief Считывает значение с клавиатуры с проверкой ввода
+* @return возвращает корректное значение
+* @throw завершает программу при некорректном вводе
 */
 double getValue();
 
 /**
- * @brief Вычисляет время, за которое путник одолел первую половину пути.
- * Функция принимает три времени и три скорости, затем вычисляет общее расстояние
- * и определяет время, за которое путник преодолел первую половину этого расстояния.
- * @param t1 Время движения с первой скоростью (часы).
- * @param v1 Первая скорость (км/ч).
- * @param t2 Время движения со второй скоростью (часы).
- * @param v2 Вторая скорость (км/ч).
- * @param t3 Время движения с третьей скоростью (часы).
- * @param v3 Третья скорость (км/ч).
- * @return Время, за которое путник одолел первую половину пути (часы).
+ * @brief Вычисляет время, за которое путник одолел первую половину пути
+ * @param t1 Время движения с первой скоростью (часы)
+ * @param v1 Первая скорость (км/ч)
+ * @param t2 Время движения со второй скоростью (часы)
+ * @param v2 Вторая скорость (км/ч)
+ * @param t3 Время движения с третьей скоростью (часы)
+ * @param v3 Третья скорость (км/ч)
+ * @return Время, за которое путник одолел первую половину пути (часы)
  */
-double calculateFirstHalfTime(const double t1, const double v1, const double t2, const double v2, const double t3, const double v3);
+double calculateFirstHalfTime(double t1, double v1, double t2, double v2, double t3, double v3);
 
 int main() 
 {
     setlocale(LC_ALL, "Russian");
     cout << "Введите данные о движении путника:" << endl;
 
-    cout << "Время движения с первой скоростью (t1): ";
-    double t1 = getValue();
+    try {
+        cout << "Время движения с первой скоростью (t1): ";
+        double t1 = getValue();
 
-    cout << "Скорость 1 (v1): ";
-    double v1 = getValue();
+        cout << "Скорость 1 (v1): ";
+        double v1 = getValue();
 
-    cout << "Время движения со второй скоростью (t2): ";
-    double t2 = getValue();
+        cout << "Время движения со второй скоростью (t2): ";
+        double t2 = getValue();
 
-    cout << "Скорость 2 (v2): ";
-    double v2 = getValue();
+        cout << "Скорость 2 (v2): ";
+        double v2 = getValue();
 
-    cout << "Время движения с третьей скоростью (t3): ";
-    double t3 = getValue();
+        cout << "Время движения с третьей скоростью (t3): ";
+        double t3 = getValue();
 
-    cout << "Скорость 3 (v3): ";
-    double v3 = getValue();
+        cout << "Скорость 3 (v3): ";
+        double v3 = getValue();
 
-    double firstHalfTime = calculateFirstHalfTime(t1, v1, t2, v2, t3, v3);
+        double firstHalfTime = calculateFirstHalfTime(t1, v1, t2, v2, t3, v3);
 
-    cout << "Время, за которое путник одолел первую половину пути: " << firstHalfTime << " часов." << endl;
+        cout << "Время, за которое путник одолел первую половину пути: " 
+             << firstHalfTime << " часов." << endl;
+    } 
+    catch (...) {
+        cerr << "Произошла ошибка при выполнении программы" << endl;
+        return 1;
+    }
 
     return 0;
 }
 
 double getValue()
 {
-    double value;
+    double value = 0.0;
     cin >> value;
-    if (cin.fail())
-    {
-        cout << "Некорректное значение" << endl;
+
+    if (cin.fail()) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cerr << "Ошибка: введено некорректное значение!" << endl;
         abort();
     }
+
+    if (value < 0) {
+        cerr << "Ошибка: значение не может быть отрицательным!" << endl;
+        abort();
+    }
+
     return value;
 }
 
-double calculateFirstHalfTime(double t1, double v1, double t2, double v2, double t3, double v3) {
-    double distance1 = t1 * v1; // Расстояние на первой части пути
-    double distance2 = t2 * v2; // Расстояние на второй части пути
-    double distance3 = t3 * v3; // Расстояние на третьей части пути
+double calculateFirstHalfTime(double t1, double v1, double t2, double v2, double t3, double v3) 
+{
+    double distance1 = t1 * v1;
+    double distance2 = t2 * v2;
+    double distance3 = t3 * v3;
 
-    double totalDistance = distance1 + distance2 + distance3; // Общее расстояние
-    double halfDistance = totalDistance / 2; // Половина пути
+    double totalDistance = distance1 + distance2 + distance3;
+    double halfDistance = totalDistance / 2;
 
-    double timeSpent = 0; // Время, затраченное на первую половину пути
+    double timeSpent = 0.0;
 
-    // Определяем, сколько времени требуется для достижения половины пути
     if (halfDistance <= distance1) {
-        timeSpent = halfDistance / v1; // Если первая половина пути укладывается в первую часть
+        timeSpent = halfDistance / v1;
     }
     else {
-        timeSpent += t1; // Учитываем все время первой части
-        halfDistance -= distance1; // Вычитаем пройденное расстояние
+        timeSpent += t1;
+        halfDistance -= distance1;
+        
         if (halfDistance <= distance2) {
-            timeSpent += halfDistance / v2; // Вторая часть пути
+            timeSpent += halfDistance / v2;
         }
         else {
-            timeSpent += t2; // Учитываем все время второй части
-            halfDistance -= distance2; // Вычитаем пройденное расстояние
-            timeSpent += halfDistance / v3; // Остаток пути на третьей части
+            timeSpent += t2;
+            halfDistance -= distance2;
+            timeSpent += halfDistance / v3;
         }
     }
 
